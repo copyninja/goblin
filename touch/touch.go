52999766e2f166524c10f3c42a9e5b468e178b48
Vasudev Kamath <kamathvasudev@gmail.com>
2011-10-09 22:24:03 +0530

    fixes: flag.Lookup removed instead normal flag parsing is used
    Program is now completely like heirloom projects touch

--- touch/touch.go
+++ touch/touch.go
@@ -15,14 +15,15 @@ var (
 	aflag = flag.Bool("a", false, "Change the access time")
 	mflag = flag.Bool("m", false, "Change the modification time")
 	cflag = flag.Bool("c", false, "Don't create the file if not exists")
-	tflag = flag.String("t", "[[CC]YY]MMDDhhmm[.SS]", "Use time instead of current time. Time specified as [[CC]YY]MMDDhhmm[.SS]")
+	tflag = flag.String("t", "time", "Use time instead of current time. Time specified as [[CC]YY]MMDDhhmm[.SS]")
 	rflag = flag.String("r", "ref_file", "Use time of corresponding file rather than current time")
 )
 
 var (
-	now      = time.Seconds()
-	errcnt   = 0
-	nulltime = false
+	now      = time.Seconds() // Current time
+	errcnt   = 0              // count errors
+	nulltime = false          // no time given in the arguments
+	datetime = true           // Use date_time operand not -t
 )
 
 func usage() {
@@ -36,20 +37,57 @@ func badtime() {
 }
 
 func atot(s string, m int) int {
-	i, e := strconv.Atoi(s)
+	i, _ := strconv.Atoi(s)
 
 	if i > m || i < 0 || s[0] == '+' || s[0] == '-' {
 		badtime()
-		fmt.Fprintf(os.Stderr, "Error: %s", e.String())
 	}
 
 	return i
+}
+
+func otime(cp string) syscall.Time_t {
+	t := now
+	stm := time.LocalTime()
+	datetime = true
+
+	stm.Second = 0
+
+	switch len(cp) {
+	case 10:
+		if stm.Year = int64(atot(cp[8:],99)); stm.Year < 69 {
+			stm.Year += 100
+		}
+		cp = cp[:8]
+	case 8:
+		stm.Minute = atot(cp[6:],59)
+		cp = cp[:6]
+		stm.Hour = atot(cp[4:],23)
+		cp = cp[:4]
+		if stm.Day = atot(cp[2:],31); stm.Day == 0 {
+			badtime()
+		}
+		cp = cp[:2]
+
+		if stm.Month = atot(cp,12); stm.Month == 0 {
+			badtime()
+		}
+		stm.Month -= 1
+
+	default:
+		badtime()
+	}
+
+	t = stm.Seconds()
+	return syscall.Time_t(t)
+
 }
 
 func ptime(cp string) syscall.Time_t {
 	t := now
 	stm := time.LocalTime()
 	sz := len(cp)
+
 
 	if sz == 11 || sz == 13 || sz == 15 {
 		if strings.Index(cp, ".") != sz-3 {
@@ -68,7 +106,7 @@ func ptime(cp string) syscall.Time_t {
 		if stm.Year = int64(atot(year, 30000)); stm.Year < 1970 {
 			badtime()
 		}
-
+		stm.Year -= 1900
 		cp = cp[4:]
 		sz -= 4
 	} else if sz == 10 {
@@ -115,7 +153,6 @@ func reffile(filename string) (syscall.Time_t, syscall.Time_t) {
 }
 
 func touch(filename string, nacc, nmod syscall.Time_t) {
-	fmt.Println("Inside Touch")
 	var st syscall.Stat_t
 	var ut syscall.Utimbuf
 
@@ -176,42 +213,32 @@ func touch(filename string, nacc, nmod syscall.Time_t) {
 func main() {
 	flag.Parse()
 
-	var nacc, nmod syscall.Time_t
-
-	//FIXME: I couldn't find equivalaent for C's multiple assignment!
-	nacc = syscall.Time_t(-1)
-	nmod = syscall.Time_t(-1)
+	nacc, nmod := syscall.Time_t(-1), syscall.Time_t(-1)
+	optind := 0
 
 	if flag.NArg() == 0 {
 		usage()
 	}
 
-	tf := flag.Lookup("t")
-	if tf != nil {
-		fmt.Println(tf.Value.String())
-		fmt.Println(len(tf.Value.String()))
-		if len(tf.Value.String()) == 0 {
-			usage()
-		}
-
-		//FIXME: I couldn't find equivalaent for C's multiple assignment!
-		acmodtime := ptime(tf.Value.String()[:])
-		nacc = acmodtime
-		nmod = acmodtime
+	if *tflag != "time" {
+		acmodtime := ptime(*tflag)
+		nacc, nmod = acmodtime, acmodtime
 	}
 
-	pf := flag.Lookup("r")
-	if pf != nil {
-		if len(pf.Value.String()) == 0 {
-			usage()
-		}
-
+	if *rflag != "ref_file" {
 		if nacc != syscall.Time_t(-1) && nmod != syscall.Time_t(-1) {
 			usage()
 		}
 
-		nacc, nmod = reffile(pf.Value.String())
-		fmt.Printf("nacc = %d\n nmod = %d\n", nacc, nmod)
+		nacc, nmod = reffile(*rflag)
+	}
+
+	firstArg := flag.Arg(0)
+	if firstArg[0] >= 0 || firstArg[0] <= 9 && nacc == syscall.Time_t(-1) && nmod == syscall.Time_t(-1) {
+		// Looks like old style argument for time stamp
+		accmodtime := otime(firstArg[:])
+		nacc, nmod = accmodtime,accmodtime
+		optind ++
 	}
 
 	if nacc == syscall.Time_t(-1) && nmod == syscall.Time_t(-1) && !*aflag && !*mflag {
@@ -230,7 +257,17 @@ func main() {
 		*aflag, *mflag = true, true
 	}
 
-	for i := 0; i < flag.NArg(); i++ {
+	if optind >= flag.NArg() && ! datetime {
+		usage()
+	}
+	
+	for i := optind; i < flag.NArg(); i++ {
 		touch(flag.Arg(i), nacc, nmod)
 	}
+
+	if errcnt < 0100 {
+		os.Exit(errcnt)
+	}
+
+	os.Exit(077)
 }
